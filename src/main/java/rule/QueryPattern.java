aa7570cc3246091b988a5370e71e9e37d6dcde1a
ndkmath1 <ndkmath1@gmail.com>
2018-04-06 12:07:23 +0700

    add range milion

--- src/main/java/rule/QueryPattern.java
+++ src/main/java/rule/QueryPattern.java
@@ -12,13 +12,21 @@ public class QueryPattern {
     private static final String SINGLE_PRICE = "(?i)((?:\\d*\\.)?\\d+)(\\s*)(t[yỷ]?)";
     private static final Pattern SINGLE_PRICE_PATTERN = Pattern.compile(SINGLE_PRICE);
     // mua chung cư giá 1-2 tỷ tại linh đàm, 1 2 tỷ, 1 - 2 t, 1.5 - 2.5 ty
-    private static final String RANGE_PRICE = "(?i)((?:\\d*\\.)?\\d+)(\\s*)([\\s-,_])(\\s*)((?:\\d*\\.)?\\d+)(\\s*)(t[yỷ]?)";
-    private static final Pattern RANGE_PRICE_PATTERN = Pattern.compile(RANGE_PRICE);
+    private static final String RANGE_PRICE_BI = "(?i)((?:\\d*\\.)?\\d+)(\\s*(t[yỷ]?)?\\s*)([\\s-,_])(\\s*)((?:\\d*\\.)?\\d+)(\\s*t[yỷ]?)";
+    private static final Pattern RANGE_PRICE_BI_PATTERN = Pattern.compile(RANGE_PRICE_BI);
+
+    private static final String RANGE_PRICE_MI = "(?i)((?:\\d*\\.)?\\d+)(\\s*(tr(i[eêệ]u)?)?\\s*)([\\s-,_])(\\s*)((?:\\d*\\.)?\\d+)(\\s*tr(i[eêệ]u)?)";
+    private static final Pattern RANGE_PRICE_MI_PATTERN = Pattern.compile(RANGE_PRICE_MI);
+
+    private static final String RANGE_PRICE_MI_BI = "";
+    private static final Pattern RANGE_PRICE_MI_BI_PATTERN = Pattern.compile(RANGE_PRICE_MI_BI);
 
     private static final String LESS_THAN_PRICE = "";
     private static final String GREATER_THAN_PRICE = "";
 
+    private static final String SINGLE_AREA = "";
     private static final Pattern SINGLE_AREA_PATTERN = Pattern.compile("");
+    private static final String RANGE_AREA = "";
     private static final Pattern RANGE_AREA_PATTERN = Pattern.compile("");
     String areaPattern = "m2";
 
@@ -47,6 +55,18 @@ public class QueryPattern {
             "mua chung cu .2 , .3 t",
             "mua chung cu 2.2 .4 ty",
             "mua chung cư giá 1.5-2.25 tỷ tại linh đàm",
+            "mua chung cu gia .25t-.3t tai linh dam",
+            "mua chung cu gia 1t-2t",
+            "mua chung cu gia 1.2tỷ-2.8tỷ",
+            "1.2 tỷ 2.8 tỷ",
+            "1.2 ty 2.3 ty",
+            "1.2 ty , 2.4 t"
+    };
+
+    private static String[] rangesMi = {
+            "1-2trieu",
+            "1-2tr",
+            "1-2trieeu"
     };
 
 
@@ -66,11 +86,12 @@ public class QueryPattern {
     }
 
     private static void matchRangePrice(String s) {
-        Matcher m = RANGE_PRICE_PATTERN.matcher(s);
+        Matcher m = RANGE_PRICE_BI_PATTERN.matcher(s);
         if (m.find()) {
+            int count = m.groupCount();
             System.out.println(m.group());
             System.out.println(m.group(1));
-            System.out.println(m.group(5));
+            System.out.println(m.group(count - 1));
         }
     }
 
